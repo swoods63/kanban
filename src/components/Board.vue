431cc0ad153e62eff9c6dba85dd98bc16da72c9e
Susan Woods <swoods63@msn.com>
2017-06-03 17:57:48 -0600

    sync

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,23 +1,75 @@
 <template>
-  <div class="board"> 
-    
-    Board name: {{board.name}}
-    Board description: {{board.description}}  
-    <!--<list v-for="list in lists" :listProp = list></list>-->
-  </div>
+  <div class="board">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div>
+            <button type="button" class="btn btn-success" @click="logout">Logout</button>
+          </div>
+        </div>
+        <div class="col-xs-4">
+          <form @submit.prevent="createList">
+          <div class="form-group">
+            <input type="text" class="form-control" v-model="name" placeholder="List Name" required>
+          </div>
+          <div class="form-group">
+            <textarea rows="4" cols="50" class="form-control" v-model="description" placeholder="Description"></textarea>
+          </div>
+            <button class="btn btn-primary" type="submit">Create List</button>
+        </form>
+        </div>
+      </div>
+      <div class="row">
+        <!--<div class="col-xs-4">
+          <div class="well">
+            <list :listProp="list"></list>
+            <h1>Board Name: {{board.name}}</h1>
+            <h2>Board description: {{board.description}}</h2>
+          </div>
+        </div>-->
+        <List v-for="list in lists" :listProp="list" class="col-xs-4"></List>
+      </div>
+    </div>
+  </div>  
 </template>
 
 <script>
 import List from "./List"
 export default {
   name: 'board',
+  data() {
+      return {
+        name: '',
+        description: '',
+        boardId: this.$store.state.activeBoard._id
+      }
+    },
   mounted(){
-    this.$store.dispatch('getBoard', (this.$route.params.id)
-    )},
+    this.$store.dispatch('getBoard', this.$route.params.id)
+    this.$store.dispatch('getLists', this.$route.params.id)
+  },
   computed:{
     board(){
       return this.$store.state.activeBoard
+    },
+    lists(){
+      return this.$store.state.activeLists
     } 
+  },
+  methods:{
+    createList(){
+      this.$store.dispatch('createlist', {
+        name: this.name,
+        boardId: this.board._id
+      })
+      this.name = ''
+    },
+    logout(){
+      this.$store.dispatch.logout()
+
+    }
+
+
   },
   components: {
     List
--- src/components/Boards.vue
+++ src/components/Boards.vue
@@ -1,35 +1,71 @@
 <template>
-  <div> 
-    <button @click="createBoard">Add Board</button>
-    <ul>
-      <li v-for="board in boards"><router-link :to="'/boards/'+board._id">{{board.name}}</router-link> <span @click="removeBoard(board)">x</span></li>
-    </ul>
+  <div class="boards">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div>
+            <button type="button" class="btn btn-success" @click="logout">Logout</button>
+          </div>
+        </div>
+        <div class="col-xs-4">
+          <form @submit.prevent="createBoard">
+            <div class="form-group">
+              <input type="text" class="form-control" v-model="name" placeholder="List Name" required>
+            </div>
+            <div class="form-group">
+              <textarea rows="4" cols="50" class="form-control" v-model="description" placeholder="Description"></textarea>
+            </div>
+            <button class="btn btn-primary" type="submit">Add Board</button>
+          </form>
+        </div>
+        <div class="col-xs-4 col-xs-offset-1" v-for="board in boards">
+          <router-link :to="'/boards/'+board._id">{{board.name}}</router-link>
+          <button @click="removeBoard(board)">X</button>
+        </div>        
+      </div>
+    </div>    
   </div>
 </template>
 
 <script>
-export default {
-  name: 'boards',
-  mounted(){
-    this.$store.dispatch('getBoards')
-  },
-  computed:{
-    boards(){
-      return this.$store.state.boards
-    }
-  },
-  methods:{
-    createBoard(){
-      this.$store.dispatch('createBoard', {
-        name: 'Testing board creation',
-        description: 'blarg'
-      })
+  //<button @click="createBoard">Add Board</button>
+  // v-for="board in boards">
+  //       <router-link :to="'/boards/'+board._id">{{board.name}}</router-link> <span @click="removeBoard(board)">x</span></li>
+  export default {
+    name: 'boards',
+    data() {
+      return {
+        name: '',
+        description: '',
+        creatorId: '',
+      }
     },
-    removeBoard(board){
-      this.$store.dispatch('removeBoard', board)
+    mounted() {
+      this.$store.dispatch('getBoards')
+    },
+    computed: {
+      boards() {
+        return this.$store.state.boards
+      }
+    },
+    methods: {
+      createBoard() {
+        this.$store.dispatch('createBoard', {
+          name: this.name,
+          description: this.description
+        })
+        this.name = ''
+        this.description = ''
+      },
+      removeBoard(board) {
+        this.$store.dispatch('removeBoard', board)
+      },
+      logout() {
+        this.$store.dispatch('logout')
+      }
     }
   }
-}
+
 </script>
 
 <style scoped>
